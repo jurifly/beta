fd1d98d9111e0c509ba53182a003c9705e1b181f
Nawal Yadav <nawal411.NS@gmail.com>
2025-07-02 15:17:14 +0000

    onlu enable these roles for devloper mode

--- src/components/dashboard/user-nav.tsx
+++ src/components/dashboard/user-nav.tsx
@@ -183,6 +183,8 @@ export function UserNav() {
                  <DropdownMenuRadioGroup value={userProfile.role} onValueChange={handleRoleChange as (value: string) => void}>
                     <DropdownMenuRadioItem value="Founder">Founder</DropdownMenuRadioItem>
                     <DropdownMenuRadioItem value="CA">Chartered Accountant</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="Legal Advisor">Legal Advisor</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="Enterprise">Enterprise</DropdownMenuRadioItem>
                  </DropdownMenuRadioGroup>
               </DropdownMenuSubContent>
             </DropdownMenuPortal>
