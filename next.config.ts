c2a8c079ef26800de2d3e715018e6e03efe7f80c
Anmol Pandhi <anmolpandhi04@gmail.com>
2025-08-17 13:37:54 +0000

    go through all files and make sure i get no errors while pushing on verc

--- next.config.ts
+++ next.config.ts
@@ -67,6 +67,9 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  serverActions: {
+    bodySizeLimit: '5mb',
+  }
 };
 
 export default nextConfig;
--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLjqD3sPPQ40gI9blVWptATS7A3I-QxIM",
-  authDomain: "lexiqaibeta.firebaseapp.com",
-  projectId: "lexiqaibeta",
-  storageBucket: "lexiqaibeta.firebasestorage.app",
-  messagingSenderId: "761844314992",
-  appId: "1:761844314992:web:3463f225d8f763905b55b5",
-  measurementId: "G-0RC823RTMB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
